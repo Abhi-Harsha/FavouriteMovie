c6ae8a8cbb5039da2a5634b2656afa381f0b5310
Abhishek H P <abhishekhp@Abhisheks-Mac.local>
2016-04-28 00:51:13 +0530

    created nsmanagedobject for movie entity

---
+++ FavouriteMovie/Movie+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Movie+CoreDataProperties.swift
+//  FavouriteMovie
+//
+//  Created by Abhishek H P on 4/28/16.
+//  Copyright © 2016 Abhishek H P. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+
+    @NSManaged var image: NSData?
+    @NSManaged var imdblink: String?
+    @NSManaged var moviedescription: String?
+    @NSManaged var plot: String?
+    @NSManaged var title: String?
+
+}
---
+++ FavouriteMovie/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  FavouriteMovie
+//
+//  Created by Abhishek H P on 4/28/16.
+//  Copyright © 2016 Abhishek H P. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Movie: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
