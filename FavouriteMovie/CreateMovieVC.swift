58260dd2dae1b92d8c5f6d72bbcfda3fab5f54c2
Abhishek H P <abhishekhp@abhisheks-mac.local>
2016-05-04 23:07:39 +0530

    added method to dismiss the UI after hitting back button

--- FavouriteMovie/CreateMovieVC.swift
+++ FavouriteMovie/CreateMovieVC.swift
@@ -6,13 +6,24 @@
 //  Copyright © 2016 Abhishek H P. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
- class CreateMovie: UIViewController {
+class CreateMovieVC: UIViewController {
+    
     @IBOutlet weak var mvtitle: UITextField!
     @IBOutlet weak var mvdescription: UITextField!
     @IBOutlet weak var mvlink: UITextField!
     @IBOutlet weak var mvplot: UITextField!
     @IBOutlet weak var mvImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onBackPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
 }
