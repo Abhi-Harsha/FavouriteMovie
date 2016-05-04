688891981f60807f1dd66e986a325185bdda22ba
Abhishek H P <abhishekhp@Abhisheks-Mac.local>
2016-05-04 12:01:21 +0530

    added create movie controller and connected outlets

---
+++ FavouriteMovie/CreateMovieVC.swift
@@ -0,0 +1,18 @@
+//
+//  CreateMovieVC.swift
+//  FavouriteMovie
+//
+//  Created by Abhishek H P on 5/4/16.
+//  Copyright © 2016 Abhishek H P. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+ class CreateMovie: UIViewController {
+    @IBOutlet weak var mvtitle: UITextField!
+    @IBOutlet weak var mvdescription: UITextField!
+    @IBOutlet weak var mvlink: UITextField!
+    @IBOutlet weak var mvplot: UITextField!
+    @IBOutlet weak var mvImage: UIImageView!
+}
