6eea69fed3107f7180dbe4ed103dda61c341cb15
Abhishek H P <abhishekhp@Abhisheks-Mac.local>
2016-05-04 23:10:31 +0530

    fixed exception and implemented method to fetch results from db

--- FavouriteMovie/ViewController.swift
+++ FavouriteMovie/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var movies = [Movie]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        getMovieResults()
+        tableView.reloadData()
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -32,12 +43,32 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
 
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 0
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
+    
+    func getMovieResults() {
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        let dbContext = appDelegate?.managedObjectContext
+        let fetchMovieResults = NSFetchRequest(entityName: "Movie")
+        
+        do {
+            let fetchedResult = try dbContext?.executeFetchRequest(fetchMovieResults)
+            movies = fetchedResult as! [Movie]
+        } catch let exception as NSError {
+            print(exception.description)
+        }
+    }
 
+    
+    
+    
+    
+    
+    
+    
 }
 
